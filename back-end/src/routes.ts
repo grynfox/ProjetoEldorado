bc021fcab264abde321db8a51f82d6e6250a0390
felipedmsantos95 <fdms@icomp.ufam.edu.br>
2021-11-06 11:29:36 -0400

    initializing backend server

---
+++ back-end/src/routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+const routes = express.Router();
+
+
+// routes.get('/itens', itensController.index);
+// routes.get('/points', pointsController.index);
+// routes.get('/points/:id', pointsController.show);
+
+
+// routes.post('/points', pointsController.create);
+
+
+export default routes;
